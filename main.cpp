c38559864fa441462dae97ef9f1058462019e070
Se7s69 <mohamed2001hussein@gmail.com>
2024-08-03 16:28:03 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <array>
+#include <vector>
+#include <cstdlib>  
+#include <ctime>    
+#include <algorithm>
+#include <limits> 
+#include <utility> 
+#include <stdexcept>
+#include <sstream>
+#include "rollingHashes.h"
+#include <fstream>
+#include <cstring>
+#include <sstream>
+#include <assert.h>
+#include <queue>
+#include <thread>
+#include <iterator>
+#include <vector>        
+#include <deque>         
+#include <list>          
+#include <array>         
+#include <map>           
+#include <unordered_map> 
+#include <set>           
+#include <unordered_set> 
+#include <stack>         
+#include <queue>         
+#include <algorithm>     
+#include <iostream>      
+#include <string>        
+
+using namespace std;
+
+void main() {
+
+	string s = "abc";
+	string t = "abc";
+	cout << s.compare(t) << endl;
+
+}
